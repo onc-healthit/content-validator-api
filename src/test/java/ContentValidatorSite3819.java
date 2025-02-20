22545ce62a7d36bc4c637ef08bdc4af8430dbf86
Girish <bgirishrao@drajer.com>
2025-02-20 17:07:24 -0500

    update test

--- src/test/java/ContentValidatorSite3819.java
+++ src/test/java/ContentValidatorSite3819.java
@@ -65,7 +65,7 @@ public class ContentValidatorSite3819 extends ContentValidatorTester {
 
 	private ArrayList<ContentValidationResult> validateDocumentAndReturnResultsSvap2022(String validationObjective,
 			String referenceFileName, String ccdaFileAsString, SeverityLevel severityLevel) {
-		return validator.validate(validationObjective, referenceFileName, ccdaFileAsString, false, true, false, severityLevel);
+		return validator.validate(validationObjective, referenceFileName, ccdaFileAsString, "uscdiv4", severityLevel);
 	}
 
 	private ArrayList<ContentValidationResult> validateDocumentAndReturnResultsSvap2022(String referenceFileName,
